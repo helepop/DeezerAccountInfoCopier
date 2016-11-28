db218403081d91231a73392011af7c375e14cc25
helepop <helepop@gmx.net>
2016-11-28 08:49:38 +0100

    complete data

--- App/lib/helper.js
+++ App/lib/helper.js
@@ -2,13 +2,13 @@
 
 var accessToken;
 
-function Start(files){
+function Start(files) {
     console.log("files: " + files);
-    for(var j = 0, file; file = files[j]; j++){        
+    for (var j = 0, file; file = files[j]; j++) {
         console.log(file);
         var fr = new FileReader();
-        fr.onloadend = function(event){
-            var res = event.target.result;             
+        fr.onloadend = function (event) {
+            var res = event.target.result;
             var content = JSON.parse(res);
             copyContent(content);
         };
@@ -16,59 +16,41 @@ function Start(files){
     }
 }
 
-function copyContent(content){
-	for(var j = 0, data; data = content.data[j]; j++){	
-        var apiCall = 'user/me/' + data.type + 's';
-        console.log('apiCall: ' + apiCall);
-        DZ.api(apiCall, 'POST', {album_id : data.id} , function(response){
-            if(response.error){
-                var error = response.error;
-                console.log(error.message);
-            }else{
-                console.log(1 + "." + "id: " + response.id + " - " + response.title);
-            }
-        });
-    } 
-}
+function copyContent(content) {
+    for (var j = 0, data; data = content.data[j]; j++) {
+        if (content.data.type != 'playlist') {
+            var apiCall = 'user/1240636962/' + data.type + 's';
+            console.log('apiCall: ' + apiCall + data.type + ": " + data.id + " " + data.title);
+            //DZ.api(apiCall, 'POST', { album_id: data.id }, function (response) {
+            //    if (response.error) {
+            //        var error = response.error;
+            //        console.log(error.message);
+            //    } else {
+            //        console.log(1 + "." + "id: " + response.id + " - " + response.title);
+            //    }
 
-function CopyAlbums(albums){
-    //console.log(albums);
-    var len = albums.data.length;
-    var albenTotal = albums.data.total
-     
-    console.log("laenge: " + len);
-    for (var i = 0; i < len; i++) {
-        var album = albums.data[i];
-        //console.log(i + "." + "id: " + album.id + " - " + album.title);
-        DZ.api('user/me/albums', album.id, function(response){
-            console.log(1 + "." + "id: " +  + " - " + response.title);
-        });
+            //});
+        }
     }
-
-    /*albums.data.forEach(function(element) {
-        console.log("i" + "id: " + element.id +  + element.title);
-    }, this);*/
 }
-
-
 
 function HandleFileSelect(evt) {
     var files = evt.target.files; // FileList object
-    
+
     Start(files);
 }
 
-function Init(){
+function Init() {
     document.getElementById('files').addEventListener('change', HandleFileSelect, false);
-    
+
     DZ.init({
-		appId  : '214962',
-		channelUrl : 'http://localhost/DeezerAccountInfoCopier/App/channel.html'
-	});
+        appId: '214962',
+        channelUrl: 'http://localhost/DeezerAccountInfoCopier/App/channel.html'
+    });
     document.getElementById('dz-init').innerHTML = 'DZ.SDK initialized';
 
     //Check if User is logged in
-    DZ.getLoginStatus(function(response) {
+    DZ.getLoginStatus(function (response) {
         if (response.authResponse) {
             console.log("logged in and connected user, someone you know");
             accessToken = response.authResponse.accessToken;
@@ -77,9 +59,11 @@ function Init(){
             Login();
         }
     });
+
+    getOwnInformtions();
 }
 
-function Login(){
+function Login() {
     DZ.login(function (response) {
         if (response.authResponse) {
             DZ.api('/user/me', function (response) {
@@ -91,4 +75,20 @@ function Login(){
             alert('User cancelled login or did not fully authorize.');
         }
     }, { perms: 'manage_community,manage_library,basic_access,email' });
+}
+
+function getOwnInformtions(userID) {
+    var res; 
+    if (userID != "") {
+        DZ.api('/user/' + userID, function (response) {
+            res = response;
+            console.log("My name", response.name);
+        });
+    } else {
+        DZ.api('/user/me', function (response) {
+            res = response;
+            console.log("My name", response.name);
+        });
+    }
+    return res;
 }
